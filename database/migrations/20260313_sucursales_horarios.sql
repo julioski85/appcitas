a12be73741101574d330ad5a05c22d4b0c499014
julioski85 <132955098+julioski85@users.noreply.github.com>
2026-03-17 00:30:52 -0600

    Delete database directory

--- database/migrations/20260313_sucursales_horarios.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE sucursales
-    ADD COLUMN hora_apertura TIME NOT NULL DEFAULT '08:00:00' AFTER capacidad_simultanea,
-    ADD COLUMN hora_cierre TIME NOT NULL DEFAULT '20:00:00' AFTER hora_apertura;
--- database/migrations/20260317_buffer_minutos_sucursales.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE sucursales
-    ADD COLUMN buffer_minutos INT NOT NULL DEFAULT 5 AFTER hora_cierre;
